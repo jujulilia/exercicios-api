8196f780a8df504a55feb3ad9c46ae3152809dce
jujulilia <jujuscosta07@gmail.com>
2023-08-01 10:17:01 -0300

    Alteração no exercicio sete

--- app/Http/Controllers/ExercicioSete.php
+++ app/Http/Controllers/ExercicioSete.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class ExercicioSete extends Controller
 {
-    //
+    public function exibirNumeros()
+    {
+        $numeros = []; 
+
+        for ($i = 1; $i <= 70; $i++) {
+          array_push($numeros, $i);
+        }
+
+        return json_encode([
+            'valores'=> $numeros
+        ]);
+    }
 }
